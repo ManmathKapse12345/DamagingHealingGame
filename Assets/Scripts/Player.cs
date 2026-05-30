c379a7e71c314541aeaa0d4a7b9dcecedc3340c6
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-30 14:40:43 +0530

    added basic animation !
    I have added animation but some more settings and refinement is required

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,11 @@ public class Player : MonoBehaviour
 {
     private float speed = 10f;
     private float rotationSpeed = 10f;
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,7 +16,21 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
-        transform.Rotate(Vector3.up*Time.deltaTime*speed*horizontalInput);
+        // animator.SetBool("ClearUpper",true);
+        if (verticalInput == 1f)
+        {
+            animator.SetBool("IdleToRun", true);
+            animator.SetBool("RunToIdle",false);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        }
+        else
+        {
+            animator.SetBool("IdleToRun", false);
+            animator.SetBool("RunToIdle", true);
+        }
+
+        // animator.SetBool("IdleToRun",false);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * horizontalInput);
     }
 }
