c8bf077b13f9ae59a404c00ec6460483f8018a9c
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-30 20:33:27 +0530

    completing the human motion scene !
    Here i have added script so that camera follow and rotate around player from back and always faces at back

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     public GameObject objectFollowed;
-    private Vector3 offset = new Vector3(0,2,-3);
+    private float rotationSpeed = 50f;
+    private Vector3 offset = new Vector3(0,2,-4);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,11 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Quaternion rotation = Quaternion.AngleAxis(horizontalInput*rotationSpeed*Time.deltaTime,Vector3.up);
+        offset = rotation*offset;
         transform.position = objectFollowed.transform.position + offset;
+        // transform.RotateAround(objectFollowed.transform.position,Vector3.up,Time.deltaTime*rotationSpeed*horizontalInput);
+        transform.LookAt(objectFollowed.transform.position);
     }
 }
